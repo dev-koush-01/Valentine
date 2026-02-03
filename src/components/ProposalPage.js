6e60f237072d6f4094337d0ae53b843306232d95
Koushik Chandra <koushikc030@gmail.com>
2026-02-04 03:10:34 +0530

    Fix: Remove unused useEffect import

--- src/components/ProposalPage.js
+++ src/components/ProposalPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 function ProposalPage({ onYes }) {
   const [yesClicked, setYesClicked] = useState(false);
